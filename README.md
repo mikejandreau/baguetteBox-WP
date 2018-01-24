3f304faa73ac0b6cd0036555b9a139adda975852
Michael R. Jandreau <mrjandre@imac-022279.colby.edu>
2018-01-24 13:32:56 -0500

    Minor updates

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Timeline Slider
+# baguetteBox.js for WordPress
+BaguetteBox.js is a fantastic pure JavaScript lightbox created by Marek Grzybek, aka [feimosi](https://github.com/feimosi/).
+
+
+
+Simple and easy to use lightbox script written in pure JavaScript.
+
 WordPress plugin that creates a horizontal timeline using the title, excerpt, and link from your list of posts.
 
 <a href="http://losaidos.com/dev/wpblank/timeline-demo/" target="_blank">View Demo</a>
@@ -17,3 +23,22 @@ I haven't found a good way to do this yet programmatically, but if you want to a
 ```
 
 Adding <code>class="timeline-return"</code> to your link will make it show up in single event pages and not on the main timeline page. Change the <code>href</code> to point to whatever page you've set to be the main timeline page.
+
+## Compatibility
+
+Desktop:
+* IE 8+
+* Chrome
+* Firefox 3.6+
+* Opera 12+
+* Safari 5+
+
+Mobile:
+* Safari on iOS
+* Chrome on Android
+
+## License
+
+Copyright (c) 2018 [feimosi](https://github.com/feimosi/)
+
+This content is released under the [MIT License](https://opensource.org/licenses/MIT).
--- gallery-slider.php
+++ gallery-slider.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name:  Gallery Slider
 * Plugin URI:   https://github.com/mikejandreau/gallery-slider
-* Description:  Plugin to display recent gallery excerpts as a slideshow. Simply add [gallery_slider] to the content area in WordPress.
+* Description:  Plugin to display recent gallery excerpts as a slideshow. Simply add [baguettebox] to the content area in WordPress.
 * Version:      1.0
 * Author:       Mike Jandreau
 * Author URI:   https://www.mikejandreau.net/
@@ -17,28 +17,28 @@
  * SET GALLERY LINKS TO MEDIA FILE
  * Ensures photo gallery media items will trigger lightbox when clicked
  */
-function baseinstall_set_gallery_links($out, $pairs, $atts) {
+function baguettebox_set_gallery_links($out, $pairs, $atts) {
     $atts = shortcode_atts( array( 
         'link' => 'file' 
         ), $atts );
     $out['link'] = $atts['link'];
     return $out;
 }
-add_filter('shortcode_atts_gallery', 'baseinstall_set_gallery_links', 10, 3);
+add_filter('shortcode_atts_gallery', 'baguettebox_set_gallery_links', 10, 3);
 
 
 
 
 
 // Register styles and scripts
-function gallery_slider_assets() {
-    wp_register_style('gallery_slider_styles', plugins_url('assets/css/gallery.min.css',__FILE__ ));
-    wp_enqueue_style('gallery_slider_styles');
+function baguettebox_assets() {
+    wp_register_style('baguettebox_styles', plugins_url('assets/css/gallery.min.css',__FILE__ ));
+    wp_enqueue_style('baguettebox_styles');
 
-    wp_register_script( 'gallery_slider_scripts', plugins_url('assets/js/gallery.min.js', __FILE__), array(),'1.1', true);
-    wp_enqueue_script('gallery_slider_scripts');
+    wp_register_script( 'baguettebox_scripts', plugins_url('assets/js/gallery.min.js', __FILE__), array(),'1.1', true);
+    wp_enqueue_script('baguettebox_scripts');
 }
-add_action( 'wp_enqueue_scripts','gallery_slider_assets');
+add_action( 'wp_enqueue_scripts','baguettebox_assets');
 
 
 
@@ -48,7 +48,7 @@ add_action( 'wp_enqueue_scripts','gallery_slider_assets');
  * RESPONSIVE VIDEO EMBED
  * Filter for adding wrappers around embedded objects
  */
-// function baseinstall_responsive_embeds( $content ) {
+// function baguettebox_responsive_embeds( $content ) {
 //     $content = preg_replace( "/<object/Si", '<div class="video-container"><object', $content );
 //     $content = preg_replace( "/<\/object>/Si", '</object></div>', $content );
     
@@ -57,7 +57,7 @@ add_action( 'wp_enqueue_scripts','gallery_slider_assets');
 //     $content = preg_replace( "/<\/iframe>/Si", '</iframe></div>', $content );
 //     return $content;
 // }
-// add_filter( 'the_content', 'baseinstall_responsive_embeds' );
+// add_filter( 'the_content', 'baguettebox_responsive_embeds' );
 
 
 
